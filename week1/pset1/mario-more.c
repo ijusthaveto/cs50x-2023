81312e4d6b564a2d889a55d6a37ed3b52e17d9d4
mightcell <139125732+ijusthaveto@users.noreply.github.com>
2023-08-13 01:59:37 +0800

    feat: mario-more version

---
+++ week1/pset1/mario-more.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 0;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= height - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
